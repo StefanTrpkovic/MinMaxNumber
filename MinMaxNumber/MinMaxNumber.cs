7d3c7c4e4191fd4b0443027a340c977335babb60
StefanTrpkovic <stefan.trpkovik@gmail.com>
2022-09-23 16:38:09 +0200

    Add project files.

---
+++ MinMaxNumber/MinMaxNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace MinMaxNumberNamespace
+{
+	public class MinMaxNumber
+	{
+		public static String smallestNumber(String str)
+		{
+			char[] numbers = str.ToCharArray();
+
+			if (!numbers.Contains('0'))
+			{
+				int smallestRightmostIndex = numbers.Length - 1;
+				int swapIndex = 0;
+
+				for (int i = numbers.Length - 2; i >= 0; i--)
+				{
+					if (numbers[i] < numbers[smallestRightmostIndex])
+					{
+						smallestRightmostIndex = i;
+					}
+				}
+
+				if (smallestRightmostIndex != 0)
+				{
+					for (int i = 0; i < numbers.Length; i++)
+					{
+						if (numbers[i] != numbers[smallestRightmostIndex] && i < smallestRightmostIndex)
+						{
+							swapIndex = i;
+							break;
+						}
+					}
+
+					char temp = numbers[swapIndex];
+					numbers[swapIndex] = numbers[smallestRightmostIndex];
+					numbers[smallestRightmostIndex] = temp;
+				}
+			}
+
+			return (new String(numbers));
+		}
+
+		public static void Main()
+		{
+			String num = "112";
+			Console.Write("Smallest number: " + smallestNumber(num));
+		}
+	}
+}
---
+++ TestMinMaxNumber/TestMinMaxNumber.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinMaxNumberNamespace;
+
+namespace TestMinMaxNumber
+{
+	[TestClass]
+	public class TestMinMaxNumber
+	{
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers0()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("1");
+
+			//Assert
+			Assert.AreEqual("1", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers1()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("11");
+
+			//Assert
+			Assert.AreEqual("11", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers2()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("112");
+
+			//Assert
+			Assert.AreEqual("112", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers3()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("1234");
+
+
+			//Assert
+			Assert.AreEqual("1234", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers4()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("4321");
+
+
+			//Assert
+			Assert.AreEqual("1324", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers5()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("121");
+
+			//Assert
+			Assert.AreEqual("112", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers6()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("5678");
+
+
+			//Assert
+			Assert.AreEqual("5678", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers7()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("9999999");
+
+			//Assert
+			Assert.AreEqual("9999999", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers8()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("9998999");
+
+			//Assert
+			Assert.AreEqual("8999999", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers9()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("963521114586");
+
+			//Assert
+			Assert.AreEqual("163521194586", res);
+		}
+
+		[TestMethod]
+		public void ShouldReturnMixAndMaxFromGivenNumbers10()
+		{
+			//Arrange
+			string res = MinMaxNumber.smallestNumber("22225113");
+
+			//Assert
+			Assert.AreEqual("12225123", res);
+		}
+	}
+}
