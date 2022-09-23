13742bddba60fa7e26f1260bc1cb487f88a0a6c8
StefanTrpkovic <stefan.trpkovik@gmail.com>
2022-09-23 17:18:39 +0200

    Small changes

--- TestMinMaxNumber/TestMinMaxNumberWithZero.cs
+++ TestMinMaxNumber/TestMinMaxNumberWithZero.cs
@@ -18,3 +18,11 @@ namespace TestMinMaxNumber
 		}
 	}
 }
+
+//1000 1020 2010 2100 2010 2100 567089 76505 356021 10325 1020 
+//1000 1002 1020 1200 1020 1200 507689 56507 156023 10235 1002
+
+//zemi ja najmalata brojka shto ne e nula. shiftaj ja levo
+//ako taa e vekje najlevo, togash shiftaj ja najdesnata nula najlevo
+//ako vekje ima nula do najlevata i najmalata brojka, togash baraj ja najbliskata pozicija kade shto nema nula od desna strana za da ja zamenis nulata
+//ako najmalata brojka e najlevo i nulata e posle nea odma, togash ke treba da se menuvaat drugite brojki
